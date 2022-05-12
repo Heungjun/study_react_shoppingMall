abed4448b85c639ea03f562b5370d43c18912c9b
Heungjun <gmdwns92@gmail.com>
2022-05-12 23:16:40 +0900

    Day 2, fakeStoreApi

--- src/pages/products/index.tsx
+++ src/pages/products/index.tsx
@@ -1,3 +1,27 @@
-const ProductList = () => <div>상품목록</div>;
+import { useQuery } from "react-query";
+import ProductItem from "../../components/product/item";
+import { fetcher, QueryKeys } from "../../queryClient";
+import { Product } from "../../types";
+
+const ProductList = () => {
+  const { data } = useQuery<Product[]>(QueryKeys.PRODUCTS, () =>
+    fetcher({
+      method: "GET",
+      path: "/products",
+    })
+  );
+
+  console.log(data);
+
+  return (
+    <div>
+      <ul>
+        {data?.map((product) => (
+          <ProductItem {...product} key={product.id} />
+        ))}
+      </ul>
+    </div>
+  );
+};
 
 export default ProductList;
