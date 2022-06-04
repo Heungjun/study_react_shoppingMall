bba7001d5d85813571660b282c81c58a8d09d41f
Heungjun <gmdwns92@gmail.com>
2022-06-04 20:01:13 +0900

    resolver

--- server/src/resolvers/product.ts
+++ server/src/resolvers/product.ts
@@ -1,23 +1,12 @@
 import { Resolver } from "./types";
 
-const mock_products = Array.from({ length: 20 }).map((_, i) => ({
-  // id: uuid(),
-  id: i + 1 + "",
-  // imageUrl: `https://placeimg.com/200/150/${i + 1}`,
-  imageUrl: `https://picsum.photos/id/${i + 10}/200/150`,
-  price: 50000,
-  title: `임시상품${i + 1}`,
-  description: `임시상세내용${i + 1}`,
-  createdAt: new Date(1634567890123 + (i + 1000 * 60 * 60 * 10)).toString(),
-}));
-
 const productResolver: Resolver = {
   Query: {
-    products: (parent, args, context, info) => {
-      return mock_products;
+    products: (parent, args, { db }, info) => {
+      return db.products;
     },
-    product: (parent, { id }, context, info) => {
-      const found = mock_products.find((item) => item.id === id);
+    product: (parent, { id }, { db }, info) => {
+      const found = db.products.find((item) => item.id === id);
       if (found) return found;
       return null;
     },
